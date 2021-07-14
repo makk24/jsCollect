2c607519b5247fd10b00f6d11e172670c934dbb0
kk <1009576562@qq.com>
2021-07-14 16:19:10 +0800

    1

--- 算法/摩尔投票/index.js
+++ 算法/摩尔投票/index.js
@@ -1,3 +1,14 @@
+/**
+ * 
+ * @param {*} nums 
+ * @returns 
+ * 
+ * 关于摩尔投票（获取超过半数的投票值）
+ * 
+ * 采用两次循环，首先是进行一次循环，遍历每个元素，有一致的+1，不一致的-1，count=0后换下一个数，最终如果count>0，在进行一次循环，计算重复的次数，判断是否超过半数，其他情况均返回-1
+ * 
+ * 
+ */
 function getMaxNum(nums){
   let current='',count=0
   for (let item of nums){
